d0e2c9f12e6b35f8940db5bcd4a88423336a5bfe
Martin Luna <maluna@technisys.com>
2022-03-25 15:58:28 -0300

    Replaced mock content with component

---
+++ src/components/JobCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-card
+      title="Job Card"
+      img-src="https://picsum.photos/600/300/?image=25"
+      img-alt="Image"
+      img-top
+      tag="article"
+    >
+      <b-card-text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </b-card-text>
+      <b-button href="#" variant="primary">Go somewhere</b-button>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JobCard",
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,76 +4,16 @@
     <b-container>
       <b-row>
         <b-col md="6" lg="3">
-          <div>
-            <b-card
-              title="Card Title"
-              img-src="https://picsum.photos/600/300/?image=25"
-              img-alt="Image"
-              img-top
-              tag="article"
-            >
-              <b-card-text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </b-card-text>
-
-              <b-button href="#" variant="primary">Go somewhere</b-button>
-            </b-card>
-          </div>
+          <job-card />
         </b-col>
         <b-col md="6" lg="3">
-          <div>
-            <b-card
-              title="Card Title"
-              img-src="https://picsum.photos/600/300/?image=25"
-              img-alt="Image"
-              img-top
-              tag="article"
-            >
-              <b-card-text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </b-card-text>
-
-              <b-button href="#" variant="primary">Go somewhere</b-button>
-            </b-card>
-          </div>
+          <job-card />
         </b-col>
         <b-col md="6" lg="3">
-          <div>
-            <b-card
-              title="Card Title"
-              img-src="https://picsum.photos/600/300/?image=25"
-              img-alt="Image"
-              img-top
-              tag="article"
-            >
-              <b-card-text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </b-card-text>
-
-              <b-button href="#" variant="primary">Go somewhere</b-button>
-            </b-card>
-          </div>
+          <job-card />
         </b-col>
         <b-col md="6" lg="3">
-          <div>
-            <b-card
-              title="Card Title"
-              img-src="https://picsum.photos/600/300/?image=25"
-              img-alt="Image"
-              img-top
-              tag="article"
-            >
-              <b-card-text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </b-card-text>
-
-              <b-button href="#" variant="primary">Go somewhere</b-button>
-            </b-card>
-          </div>
+          <job-card />
         </b-col>
       </b-row>
     </b-container>
@@ -81,10 +21,10 @@
 </template>
 
 <script>
-// @ is an alias to /src
+import JobCard from "../components/JobCard.vue";
 
 export default {
   name: "HomeView",
-  components: {},
+  components: { "job-card": JobCard },
 };
 </script>
