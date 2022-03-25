c234056396019c426077c87528c8ed5c86bc6fa3
Martin Luna <maluna@technisys.com>
2022-03-25 16:06:26 -0300

    Added new header component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <app-header />
     <router-view />
   </div>
 </template>
+
+<script>
+import AppHeader from "./components/layout/AppHeader.vue";
+export default {
+  components: {
+    "app-header": AppHeader,
+  },
+};
+</script>
 
 <style>
 #app {
---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>site title</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
