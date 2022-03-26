72dd28f8c954ace0f4ab53c63848e805022c41e6
Martin Luna <uxmoon@gmail.com>
2022-03-26 00:02:40 -0300

    Fetch data and loop data, pass prop name to child component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,18 +2,7 @@
   <div class="home">
     <b-container>
       <b-row>
-        <b-col md="6" lg="3">
-          <job-card />
-        </b-col>
-        <b-col md="6" lg="3">
-          <job-card />
-        </b-col>
-        <b-col md="6" lg="3">
-          <job-card />
-        </b-col>
-        <b-col md="6" lg="3">
-          <job-card />
-        </b-col>
+        <job-card v-for="job in jobs" :key="job.id" :name="job.name" />
       </b-row>
     </b-container>
   </div>
@@ -25,5 +14,21 @@ import JobCard from "../components/JobCard.vue";
 export default {
   name: "HomeView",
   components: { "job-card": JobCard },
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  methods: {
+    async fetchData() {
+      const res = await fetch("jobs.json");
+      const val = await res.json();
+      // console.log(val);
+      this.jobs = val;
+    },
+  },
+  created() {
+    this.fetchData();
+  },
 };
 </script>
