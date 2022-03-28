0b673b914e212812778246b554d424edc4aa6342
Martin Luna <maluna@technisys.com>
2022-03-28 11:40:35 -0300

    Added and setup pagination component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,15 @@
       <b-row>
         <job-card v-for="job in jobs" :key="job.id" :name="job.name" />
       </b-row>
+      <b-pagination
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        first-text="First"
+        prev-text="Prev"
+        next-text="Next"
+        last-text="Last"
+      ></b-pagination>
     </b-container>
   </div>
 </template>
@@ -17,14 +26,21 @@ export default {
   data() {
     return {
       jobs: [],
+      currentPage: 1,
+      perPage: 3,
+      rows: 1,
     };
   },
   methods: {
     async fetchData() {
+      // fetch jobs
       const res = await fetch("jobs.json");
       const val = await res.json();
-      // console.log(val);
+      // set 'jobs' data
       this.jobs = val;
+      console.log(val);
+      // set pagination rows
+      this.rows = this.jobs.length;
     },
   },
   created() {
